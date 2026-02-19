cb883da4ec5c9f906937e97264ee6eaa8745c042
Rodolfo Nei <rodolfonei.webdev@gmail.com>
2026-02-19 19:52:07 -0300

    Add project files.

---
+++ FirstAPI/Controllers/BooksController.cs
@@ -0,0 +1,76 @@
+﻿using FirstAPI.Data;
+using FirstAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly FirstAPIContext _context;
+        public BooksController(FirstAPIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> GetBooks()
+        {
+            return Ok(await _context.Books.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBookById(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+                return NotFound();
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> AddBook(Book newBook)
+        {
+            if (newBook == null)
+                return BadRequest();
+
+            _context.Books.Add(newBook);
+            await _context.SaveChangesAsync(); 
+
+            return CreatedAtAction(nameof(GetBookById), new { id = newBook.Id }, newBook);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBook(int id, Book updatedBook)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+                return NotFound();
+
+            book.Id = updatedBook.Id;
+            book.Title = updatedBook.Title;
+            book.Author = updatedBook.Author;
+            book.YearPublished = updatedBook.YearPublished;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+                return NotFound();
+
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+            
+            return NoContent();
+        }
+    }
+}
---
+++ FirstAPI/Data/FirstAPIContext.cs
@@ -0,0 +1,12 @@
+﻿using FirstAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstAPI.Data
+{
+    public class FirstAPIContext : DbContext
+    {
+        public FirstAPIContext(DbContextOptions<FirstAPIContext> options): base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
