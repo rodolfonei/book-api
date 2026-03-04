11b4135aff1bb983faa977d8919b9d3dc472c6f3
Rodolfo Nei <rodolfonei.webdev@gmail.com>
2026-03-04 17:48:03 -0300

    .NET downgrade to AWS Lambda Tools compatibility, removed OpenAPI and added Swagger

--- FirstAPI/Program.cs
+++ FirstAPI/Program.cs
@@ -1,25 +1,23 @@
-using Amazon.Lambda.Serialization.SystemTextJson;
 using FirstAPI.Data;
-using FirstAPI.Serialization;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi, 
-    new SourceGeneratorLambdaJsonSerializer<AppJsonSerializerContext>());
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddDbContext<FirstAPIContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
