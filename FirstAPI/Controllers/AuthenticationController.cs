6aadcb966503bcced74ae293c47ffee079ed8bd3
Rodolfo Nei <rodolfonei.webdev@gmail.com>
2026-03-17 16:54:55 -0300

    add auth register action

---
+++ FirstAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,54 @@
+﻿using FirstAPI.Data;
+using FirstAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly FirstAPIContext _context;
+        private readonly IConfiguration _configuration;
+    
+        public AuthenticationController(UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            FirstAPIContext context,
+            IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
+            _configuration = configuration; 
+        }
+
+        [HttpPost("register-user")]
+        public async Task<IActionResult> Register([FromBody]Register payload)
+        {
+            var userExists = await _userManager.FindByEmailAsync(payload.Email);
+
+            if (userExists != null)
+            {
+                return BadRequest($"User {payload.Email} already exists");
+            }
+
+            ApplicationUser newUser = new ApplicationUser()
+            {
+                Email = payload.Email,
+                UserName = payload.UserName,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var result = await _userManager.CreateAsync(newUser, payload.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest("User could not be created!");
+            }
+
+            return Created(nameof(Register), $"User {payload.Email} created");
+        }
+    }
+}
--- FirstAPI/Program.cs
+++ FirstAPI/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
 using System.Text;
 
 
